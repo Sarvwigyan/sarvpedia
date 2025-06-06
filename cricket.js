984616067dd3474d26a79e5b2d980c5c1efef06d
Sarvwigyan <trust.int.org@gmail.com>
2025-06-06 23:28:59 +0530

    Add files via upload

---
+++ cricket.js
@@ -0,0 +1,8 @@
+const cricketData = [
+    {
+        "title": "Cricket",
+        "link": "Cricket/Cricketo.html",
+        "tags": ["cricket", "game", "batting", "sport"],
+        "description": "Cricket lovers, you are at the right place!"
+    }
+];
